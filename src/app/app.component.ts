77184dc93764bc9f49cfc93ad7e3a13efb3b95fe
Evgeny Nagimov <enagimov@crexi.com>
2022-05-12 21:38:57 -0400

    updated packges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import {BackendService} from './backend.service';
+import { BackendService } from './backend.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   tickets = this.backend.tickets();
